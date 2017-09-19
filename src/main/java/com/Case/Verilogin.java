8fbdff124997a59090d600f57c1d9f8b5513571f
cwh <2358169776@qq.com>
2017-09-19 17:37:43 +0800

    ww

--- src/main/java/com/Case/Verilogin.java
+++ src/main/java/com/Case/Verilogin.java
@@ -17,49 +17,49 @@ public class Verilogin extends login_Logic {
 		getvericode("1376543234");
 		 name.printResult();
 	 } 
-//	@Test
-//	public void case34() {
-//		getvericode("23790887788");
-//		 name.printResult();
-//	 } 
-//	@Test
-//	public void case35() { 	 
-//		getvericode("11111112222");
-//		 name.printResult();
-//	 } 
-//	@Test
-//	public void case36() { 	 
-//		longinput("134234323432");
-//		 name.printResult();
-//	 } 
-//	@Test
-//	public void case37() { 	 
-//		isbtngrey("13423432343","");
-//		 name.printResult();
-//	 } 
-//	@Test
-//	public void case38() { 	 
-//		isbtngrey("13423432343","1111");
-//		 name.printResult();
-//	 } 
-//	@Test
-//	public void case39() { 	 
-//		snslogin(phoneNum(),0);
-//		 name.printResult();
-//	 } 
-//	@Test
-//	public void case40() { 	 
-//		correctInput(pn);
-//		 name.printResult();
-//	 } 
-//	@Test
-//	public void case41() { 	 
-//		snslogin(pn,1);
-//		 name.printResult();
-//	 } 
-//	@Test
-//	public void case42() { 	 
-//		oldlogin("17700000002");
-//		 name.printResult();
-//	 } 
+	@Test
+	public void case34() {
+		getvericode("23790887788");
+		 name.printResult();
+	 } 
+	@Test
+	public void case35() { 	 
+		getvericode("11111112222");
+		 name.printResult();
+	 } 
+	@Test
+	public void case36() { 	 
+		longinput("134234323432");
+		 name.printResult();
+	 } 
+	@Test
+	public void case37() { 	 
+		isbtngrey("13423432343","");
+		 name.printResult();
+	 } 
+	@Test
+	public void case38() { 	 
+		isbtngrey("13423432343","1111");
+		 name.printResult();
+	 } 
+	@Test
+	public void case39() { 	 
+		snslogin(phoneNum(),0);
+		 name.printResult();
+	 } 
+	@Test
+	public void case40() { 	 
+		correctInput(pn);
+		 name.printResult();
+	 } 
+	@Test
+	public void case41() { 	 
+		snslogin(pn,1);
+		 name.printResult();
+	 } 
+	@Test
+	public void case42() { 	 
+		oldlogin("17700000002");
+		 name.printResult();
+	 } 
 }
